b89f36dd989ebbd4fc8fa613926cfcd309d77279
HuuSang15 <phamhuusang24@gmail.com>
2023-10-06 16:11:12 +0700

    Header + Footer + About Page + Lien He + Login + SignUp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { router } from './routes/index';
+import { Layout } from './layouts/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Routes>
+            <Route element={<Layout hideHeaderPaths={['/login' , '/signup' , '/forgot-password']} />}>
+                {router.map((route, index) => {
+                    return <Route key={index} path={route.path} element={
+                            <route.component />
+                    }></Route>;
+                })}
+            </Route>
+        </Routes>
+    </>
   );
 }
 
